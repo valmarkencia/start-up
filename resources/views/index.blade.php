c253f07878e182ddd173d473480f8a048b109045
mark anthony valencia <valmarkencia@gmail.com>
2016-11-26 11:29:27 +0800

    start of code

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Graphic Designer — Mark Anthony Valencia</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="fold">
+		<div class="top-bar">
+			<div class="container">
+				<nav class="pull-right">
+					<ul>
+						<li class="active"><a href="/">Home</a></li>
+						<li><a href="portfolio">Portfolio</a></li>
+						<li><a href="contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="body-container container">
+			<h1 class="title">Welcome!</h1>
+			<div class="greeting">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam massa, porttitor vitae finibus non, viverra id sapien. Proin sodales erat eget orci semper feugiat. Sed diam odio, auctor vel tincidunt quis, hendrerit vel augue.
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portfolio — Mark Anthony Valencia</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="fold">
+		<div class="top-bar">
+			<div class="container">
+				<nav class="pull-right">
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li class="active"><a href="portfolio">Portfolio</a></li>
+						<li><a href="contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="container body-content">
+			<div class="row fix-250">
+				<div class="col-lg-6">
+					<div class="greeting">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis nulla at mollis rhoncus. Aenean imperdiet tristique nisi vitae sagittis. Etiam luctus turpis id porttitor interdum. Nulla tincidunt dapibus hendrerit. Aenean mollis felis purus. Mauris ut sem ac ipsum sollicitudin vestibulum ut ut est. Vestibulum nec porttitor nisl.
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+					<div class="row">
+						<div class="col-lg-5 image-content">
+							<a href="#">
+								<img src="img/trifold-zoowise.jpg" alt="zoowise trifold brochure design" title="zoowise trifold brochure design">
+							</a>
+						</div>
+						<div class="col-lg-7 content">
+							<a href="#"><h3 class="title">Zoowise Trifold Brochure</h3></a>
+							<!-- <div class="description">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis nulla at mollis rhoncus. Aenean imperdiet tristique nisi vitae sagittis.
+							</div> -->
+							<hr>
+							<div class="detail">
+							<div class="row">
+								<div class="col-xs-4">
+									<span class="identifier">
+									Software : 
+									</span>
+								</div>
+								<div class="col-xs-7 identifier">
+									<img src="img/ps-logo.png"/> <img src="img/ai-logo.png"/>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-4">
+									<span class="identifier">
+									Material : 
+									</span>
+								</div>
+								<div class="col-xs-7">
+									A4
+								</div>					
+							</div>
+							<div class="row">
+								<div class="col-xs-4">
+									<span class="identifier">
+									Client : 
+									</span>
+								</div>
+								<div class="col-xs-7">
+									<a href="http://zoowise.org" target="_blank">zoowise.org</a>
+								</div>					
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row fix-250">
+				<div class="col-lg-6">
+				</div>
+
+				<div class="col-lg-6">
+					<div class="row">
+						<div class="col-lg-5 image-content">
+							<a href="#">
+							<img src="img/flyer-zoowise.jpg" alt="zoowise flyer design" title="zoowise flyer design"></a>
+						</div>
+						
+						<div class="col-lg-7 content">
+								<a href="#"><h3 class="title">Flyer</h3></a>
+							<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis nulla at mollis rhoncus. Aenean imperdiet tristique nisi vitae sagittis. Etiam luctus turpis id porttitor interdum. Nulla tincidunt dapibus hendrerit. Aenean mollis felis purus. Mauris ut sem ac ipsum sollicitudin vestibulum ut ut est. Vestibulum nec porttitor nisl.</div>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+</body>
+</html>
