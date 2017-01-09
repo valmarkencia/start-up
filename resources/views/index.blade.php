051647b6b24f791aaf8f01660411d2d73ff579bf
mark anthony valencia <valmarkencia@gmail.com>
2017-01-09 18:02:04 +0800

    v2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,99 @@
 <head>
 	<title>Graphic Designer — Mark Anthony Valencia</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div class="fold">
-		<div class="top-bar">
-			<div class="container">
-				<nav class="pull-right">
-					<ul>
-						<li class="active"><a href="/">Home</a></li>
-						<li><a href="portfolio">Portfolio</a></li>
-						<li><a href="contact">Contact</a></li>
-					</ul>
-				</nav>
+	<div class="container">
+		<!-- nav part -->
+		<div class="nav brand">
+			<a href=""><span class="bold">Mark</span>Valencia</a>
+			<div class="pull-right">
+				<ul>
+					<li><a href="#">Home</a></li>
+				</ul>
 			</div>
 		</div>
-		<div class="body-container container">
-			<h1 class="title">Welcome!</h1>
-			<div class="greeting">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam massa, porttitor vitae finibus non, viverra id sapien. Proin sodales erat eget orci semper feugiat. Sed diam odio, auctor vel tincidunt quis, hendrerit vel augue.
+		<!-- end of nav -->
+		<!-- latest project section -->
+		<div class="latest-works section">
+			<h2>LATEST WORKS</h2>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<img src="/img/homepage2.jpg">
+					</div>
+
+					<div class="description">
+						<span class="title"><img src="/img/perocom-logo.png"></span>
+						Perocom website mockup. An auto reseller company that wants to redesign their site from a templated design.
+					</div>
+
+					<a class="pull-right success btn" href="javascript:">More Details >></a>
+				</div>
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<img src="/img/home-page-2nd-concept.jpg">
+					</div>
+
+					<div class="description">
+						<span class="title"><img src="/img/little.png"></span>
+						Little Monessori preschool wants to revamp their website to a more fun, playful and creative design.
+					</div>
+
+					<a class="pull-right success btn" href="javascript:">More Details >></a>
+				</div>
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<img src="/img/home.jpg">
+					</div>
+
+					<div class="description">
+						<span class="title"><img src="/img/ftl-logo.png"></span>
+						Perocom website mockup. An auto reseller company that wants to redesign their site from a templated design.
+					</div>
+
+					<a class="pull-right success btn" href="javascript:">More Details >></a>
+				</div>
+			</div>
+		</div>
+		<!-- end of latest projects section -->
+
+		<hr>
+
+		<!-- start of who am i section -->
+		<div class="who-am-i section">
+			<div class="profile">
+				<img src="/img/resize.jpg">
+			</div>
+			<h2>ABOUT ME</h2>
+			<div class="aboutme">
+				<span>
+					I am Mark Anthony Valencia, from Philippines. I am a Web Developer, Web Designer, UI/UX Designer and a Graphic Designer. I’ve been a Web Designer in a company for more than a year and now I am making my way up as a freelancer and challenge myself on how to deal with the clients’ needs and feedbacks. I went to different trainings to enhance my skills and to serve my clients well in providing a clean, user friendly and efficient Web design. 
+				</span>
+			</div>
+		</div>
+		<!-- end of who am I section -->
+		
+		<hr>
+
+		<div class="my-services section">
+			<h2>MY SERVICES</h2>
+			<div class="row">
+				<div class="col-md-3">
+					<img class="services" src="/img/webdes.png">
+					<h3>WEB DESIGN</h3>
+				</div>
+				<div class="col-md-3">
+					<img class="services" src="/img/graphicdes.png">
+				</div>
+				<div class="col-md-3">
+					<img class="services" src="/img/webdes.png">
+				</div>
+				<div class="col-md-3">
+					<img class="services" src="/img/webdes.png">
+				</div>
 			</div>
 		</div>
 	</div>
