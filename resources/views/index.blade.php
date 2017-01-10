f1ae7365033e50027c19a03f1514bc74ce2b2df4
mark anthony valencia <valmarkencia@gmail.com>
2017-01-10 10:49:47 +0800

    banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,69 +11,74 @@
 	<div class="container">
 		<!-- nav part -->
 		<div class="nav brand">
-			<a href="javascript:"><img class="logo" src="/img/logo.png"><span class="bold">Mark</span>Valencia</a>
+			<a href="/"><img class="logo" src="/img/logo.png"><span class="bold">MARK</span>VALENCIA</a>
 			<div class="pull-right">
 				<ul>
-					<li><a href="#">Home</a></li>
+					<li><a href="/">Home</a></li>
 				</ul>
 			</div>
 		</div>
 		<!-- end of nav -->
+	</div>
 		<!-- latest project section -->
 		<div class="latest-works section">
-			<h2>LATEST WORKS</h2>
-			<div class="row">
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img src="/img/homepage2.jpg">
-					</div>
+			<div class="container">
+				<h2><span class="bold">LATEST</span> WORKS</h2>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img src="/img/homepage2.jpg">
+						</div>
+						<div class="desc-container">
+							<div class="description">
+								<span class="title"><img src="/img/perocom-logo.png"></span>
+								Perocom Auto Pte Ltd wants to revamp their site from their old templated design.
+							
+							<a class="pull-right success btn" href="javascript:">More Details >></a>
+							</div>
+						</div>
 
-					<div class="description">
-						<span class="title"><img src="/img/perocom-logo.png"></span>
-						Perocom website mockup. An auto reseller company that wants to redesign their site from a templated design.
 					</div>
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img src="/img/home-page-2nd-concept.jpg">
+						</div>
+						<div class="desc-container">
+							<div class="description">
+								<span class="title"><img src="/img/little.png"></span>
+								Little Monessori preschool wants to revamp their website to a more fun, playful and creative design.
 
-					<a class="pull-right success btn" href="javascript:">More Details >></a>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img src="/img/home-page-2nd-concept.jpg">
+							<a class="pull-right success btn" href="javascript:">More Details >></a>
+							</div>
+						</div>
 					</div>
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<img src="/img/home.jpg">
+						</div>
+						<div class="desc-container">
+							<div class="description">
+								<span class="title"><img src="/img/ftl-logo.png"></span>
+								Freelancer Training London required me to do a wordpress design that will suit his business.
 
-					<div class="description">
-						<span class="title"><img src="/img/little.png"></span>
-						Little Monessori preschool wants to revamp their website to a more fun, playful and creative design.
+							<a class="pull-right success btn" href="javascript:">More Details >></a>
+							</div>
+						</div>
 					</div>
-
-					<a class="pull-right success btn" href="javascript:">More Details >></a>
-				</div>
-				<div class="col-md-4">
-					<div class="thumbnail">
-						<img src="/img/home.jpg">
-					</div>
-
-					<div class="description">
-						<span class="title"><img src="/img/ftl-logo.png"></span>
-						Perocom website mockup. An auto reseller company that wants to redesign their site from a templated design.
-					</div>
-
-					<a class="pull-right success btn" href="javascript:">More Details >></a>
 				</div>
 			</div>
 		</div>
 		<!-- end of latest projects section -->
-
-		<hr>
-
+		<div class="container">
 		<!-- start of who am i section -->
 		<div class="who-am-i section">
 			<div class="profile">
 				<img src="/img/resize.jpg">
 			</div>
-			<h2>ABOUT ME</h2>
+			<h2><span class="bold">ABOUT</span> ME</h2>
 			<div class="aboutme">
 				<span>
-					I am Mark Anthony Valencia, from Philippines. I am a Web Developer, Web Designer, UI/UX Designer and a Graphic Designer. I’ve been a Web Designer in a company for more than a year and now I am making my way up as a freelancer and challenge myself on how to deal with the clients’ needs and feedbacks. I went to different trainings to enhance my skills and to serve my clients well in providing a clean, user friendly and efficient Web design. 
+					My name is Mark Anthony Valencia and I am a full-time freelance web designer specialized in designing web pages efficiently. I have been in the field for almost 2 years now.
 				</span>
 			</div>
 		</div>
